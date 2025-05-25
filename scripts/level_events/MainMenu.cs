70627f9ebaa671fcb1bf6387f88cf647087bc2c8
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-05-25 22:41:45 +0300

    Ported to Godot 3.6

--- scripts/level_events/MainMenu.cs
+++ scripts/level_events/MainMenu.cs
@@ -2,37 +2,37 @@ using Godot;
 
 namespace Project
 {
-    public class MainMenu : Control
-    {
-        // Nodes
-        private RichTextLabel credit;
-        private AnimationPlayer backgroundMovement;
+	public class MainMenu : Control
+	{
+		// Nodes
+		private RichTextLabel credit;
+		private AnimationPlayer backgroundMovement;
 
-        public override void _Ready()
-        {
-            // Getting nodes
-            credit = GetNode<RichTextLabel>("Credit");
-            backgroundMovement = GetNode<AnimationPlayer>("BackgroundMovement");
+		public override void _Ready()
+		{
+			// Getting nodes
+			credit = GetNode<RichTextLabel>("Credit");
+			backgroundMovement = GetNode<AnimationPlayer>("BackgroundMovement");
 
-            // Doing stuff with nodes
-            credit.Connect("meta_clicked", this, nameof(_OnLinkClicked));
+			// Doing stuff with nodes
+			credit.Connect("meta_clicked", this, nameof(_OnLinkClicked));
 
-            // Min res so UI doesn't scale weirdly
-            OS.MinWindowSize = new Vector2(1200, 650);
+			// Min res so UI doesn't scale weirdly
+			OS.MinWindowSize = new Vector2(1200, 650);
 
-            // For when the game resets
-            Input.MouseMode = Input.MouseModeEnum.Visible;
+			// For when the game resets
+			Input.MouseMode = Input.MouseModeEnum.Visible;
 
-            // Maximizing window because yes
-            OS.WindowMaximized = true;
+			// Maximizing window because yes
+			OS.WindowMaximized = true;
 
-            // Animation player go brr
-            backgroundMovement.Play("BackgroundMovement");
-        }
+			// Animation player go brr
+			backgroundMovement.Play("BackgroundMovement");
+		}
 
-        public void _OnLinkClicked(string meta)
-        {
-            OS.ShellOpen(meta);
-        }
-    }
+		public void _OnLinkClicked(string meta)
+		{
+			OS.ShellOpen(meta);
+		}
+	}
 }
